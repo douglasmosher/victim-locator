247dd1cb024e5b15fb3e01c8b88df68563fcedaf
douglasmosher <doug.a.mosher@gmail.com>
2016-02-05 15:07:26 -0800

    Ffmpeg fix attempt 1

--- bin/ffmpeg_test.sh
+++ bin/ffmpeg_test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #https://trac.ffmpeg.org/wiki/Encode/MPEG-4
 
-/home/root/bin/ffmpeg/ffmpeg -s 1280x800 -f video4linux2 -i /dev/video0 -s 1280x800 -f video4linux2 -i /dev/video1 /
--filter_complex /
-"nullsrc=size=2560x400 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x800 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x800 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" /
+/home/root/bin/ffmpeg/ffmpeg -s 1280x800 -f video4linux2 -i /dev/video0 -s 1280x800 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=2560x400 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x800 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x800 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" /
 -f mpeg4 -b:v 500k -r 5 testy.mp4
