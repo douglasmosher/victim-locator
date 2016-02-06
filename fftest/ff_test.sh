0bc2442532978614ced246a846d3af38148dd41c
douglasmosher <doug.a.mosher@gmail.com>
2016-02-05 22:47:33 -0800

    2

--- fftest/ff_test.sh
+++ fftest/ff_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #https://trac.ffmpeg.org/wiki/Encode/MPEG-4
 
-/home/root/bin/ffmpeg/ffmpeg -i vid1.mp4 -i vid2.mp4 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -vcodec libx264 out.mp4
+/home/root/bin/ffmpeg/ffmpeg -i vid1.mp4 -i vid2.mp4 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -c:v libx264 out.mp4
