106cf5d1c36493dc5ac5f6012cbe4324e0975db2
douglasmosher <doug.a.mosher@gmail.com>
2016-02-06 21:43:31 -0800

    revert

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #https://trac.ffmpeg.org/wiki/Encode/MPEG-4
 
-/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -c:v libx264 -c:a aac -strict experimental -tune zerolatency -movflags +faststart -b:v 500k -f mp4 rtmp://192.168.0.22:8082
+/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -c:v libx264 -c:a aac -strict experimental -tune zerolatency -movflags +faststart -b:v 500k -f mp4 http://192.168.0.22:8082
