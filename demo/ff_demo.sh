66022ba3d3ef401e25bf7276c1d52b25f232135c
douglasmosher <doug.a.mosher@gmail.com>
2016-02-06 22:53:56 -0800

    demo mp4

--- demo/ff_demo.sh
+++ demo/ff_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 -t 00:00:10 output1 -t 00:00:10 output2
+/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 -c:v libx264 -c:a aac -strict experimental -b:v 1000k -f mp4 -t 00:00:10 output1.mp4 -c:v libx264 -c:a aac -strict experimental -b:v 1000k -f mp4 -t 00:00:10 output2.mp4
