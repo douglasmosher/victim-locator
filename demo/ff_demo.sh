778df4641e1bb0eab5f0d2d1f8ac8034ed735bd1
douglasmosher <doug.a.mosher@gmail.com>
2016-02-07 22:58:39 -0800

    Demo

--- demo/ff_demo.sh
+++ demo/ff_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 outpu11.mp4 output22.mp4
+/home/root/bin/ffmpeg/ffmpeg -i in1.mp4 -i in2.mp4 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -t 20 out1.mp4
