ce27126b39a7578e9ce57b966f6d8c40f685d35e
douglasmosher <doug.a.mosher@gmail.com>
2016-02-10 23:18:46 -0800

    try mp4

--- demo/ff_demo.sh
+++ demo/ff_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -i in1.mp4 -i in2.mp4 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -t 20 out1.mp4
+/home/root/bin/ffmpeg/ffmpeg -i in_1.mp4 -i in_2.mp4 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -c:v libx264 -c:a aac -strict experimental -tune zerolatency -movflags +faststart -b:v 1000k -f mp4 -t 30 out.mp4
