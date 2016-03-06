c0234d570db7d2f971494af9402f6e401fdc8901
douglasmosher <doug.a.mosher@gmail.com>
2016-03-06 14:45:10 -0800

    out2

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -5,4 +5,4 @@
 #/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" --c:v libvpx-vp9 -crf 62 -b:v 500k -c:a libvorbis http://192.168.0.22:8082/out.mp4
 
 echo "Did we make it here?"
-/home/root/bin/ffmpeg/ffmpeg -i vid1.mp4 /home/root/victim-locator/web/client/ts/out.m3u8
+/home/root/bin/ffmpeg/ffmpeg -i vid1.mp4 /home/root/victim-locator/web/client/ts/out_1.m3u8
